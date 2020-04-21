59d873297a194baa603c20447eb7d01280074ca8
Joel Detweiler <joel.detweiler@gmail.com>
2020-04-20 18:28:37 -0700

    Format daily and hourly forecast in getWeather result

--- src/api/weather.js
+++ src/api/weather.js
@@ -10,6 +10,10 @@ const api = axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5'
 });
 
+const convertDate = (dt, timezone) => utcToZonedTime(dt*1000, timezone);
+
+const getIconUrl = (weather, useLarge) => `http://openweathermap.org/img/wn/${weather[0].icon}${useLarge? '@2x' : ''}.png`;
+
 const extractCurrentWeather = ({ dt, temp, feels_like, humidity, uvi, wind_speed, weather }, timezone) => ({
   temperature: temp,
   description: weather[0].description,
@@ -17,9 +21,32 @@ const extractCurrentWeather = ({ dt, temp, feels_like, humidity, uvi, wind_speed
   humidity: humidity,
   wind: wind_speed,
   uvIndex: uvi,
-  icon: `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`,
-  date: utcToZonedTime(dt*1000, timezone),
+  icon: getIconUrl(weather, true),
+  date: convertDate(dt, timezone),
 });
+
+const extractDailyForecast = (daily, timezone) => {
+  return daily.map(({ dt, sunrise, sunset, temp, humidity, weather, uvi }) => ({
+    min: temp.min,
+    max: temp.max,
+    description: weather[0].description,
+    humidity: humidity,
+    uvIndex: uvi,
+    icon: getIconUrl(weather),
+    date: convertDate(dt, timezone),
+    sunrise: convertDate(sunrise, timezone),
+    sunset: convertDate(sunset, timezone),
+  }));
+};
+
+const extractHourlyForecast = (hourly, timezone) => {
+  return hourly.map(({ dt, temp, weather }) => ({
+    temperature: temp,
+    description: weather[0].description,
+    icon: getIconUrl(weather),
+    date: convertDate(dt, timezone),
+  }));
+};
 
 export function getWeather({ lat, lon }, units='imperial') {
   return api.get('/onecall', {
@@ -31,6 +58,8 @@ export function getWeather({ lat, lon }, units='imperial') {
     }
   }).then(({ data }) => data).then(({ current, hourly, daily, timezone }) => ({
     current: extractCurrentWeather(current, timezone),
+    daily: extractDailyForecast(daily, timezone),
+    hourly: extractHourlyForecast(hourly, timezone),
   }));
 }
 
--- src/api/weather.test.js
+++ src/api/weather.test.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import * as dateFns from 'date-fns';
+import * as _ from 'lodash';
+
 import * as weatherApi from './weather';
 
 describe('Weather API', () => {
@@ -41,6 +43,58 @@ describe('Weather API', () => {
         );
         expect(isFromPastHour).toBe(true);
       })
-    })
+    });
+    
+    describe('daily weather', () => {
+      let daily;
+      beforeAll(() => daily = result.daily);
+
+      it('should return 7 days of forecasts + today', () => {
+        expect(daily.length).toEqual(8);
+      });
+
+      it.each(_.times(8))('should format data correctly for day %#', (dayIndex) => {
+        expect(daily[dayIndex]).toEqual({
+          min: expect.any(Number),
+          max: expect.any(Number),
+          description: expect.any(String),
+          humidity: expect.any(Number),
+          uvIndex: expect.any(Number),
+          icon: expect.any(String),
+          date: expect.any(Date),
+          sunrise: expect.any(Date),
+          sunset: expect.any(Date),
+        });
+      });
+
+      it('should have a final date that is for more than 6 days from now', () => {
+        const lastDate = daily[7].date;
+        const isMoreThan7DaysAway = dateFns.isAfter(lastDate, dateFns.addDays(new Date(), 6));
+        expect(isMoreThan7DaysAway).toBe(true);
+      });
+    });
+    describe('hourly weather', () => {
+      let hourly;
+      beforeAll(() => hourly = result.hourly);
+
+      it('should return 48 hours of forecasts', () => {
+        expect(hourly.length).toEqual(48);
+      });
+
+      it.each(_.times(48))('should format data correctly for hour %#', (hourIndex) => {
+        expect(hourly[hourIndex]).toEqual({
+          temperature: expect.any(Number),
+          description: expect.any(String),
+          icon: expect.any(String),
+          date: expect.any(Date),
+        });
+      });
+
+      it('should have a final date that is for more than 24 hours from now', () => {
+        const lastDate = hourly[47].date;
+        const isMoreThan24HoursAway = dateFns.isAfter(lastDate, dateFns.add(new Date(), { hours: 24 }));
+        expect(isMoreThan24HoursAway).toBe(true);
+      });
+    });
   });
 });
